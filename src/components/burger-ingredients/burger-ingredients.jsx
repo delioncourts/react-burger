e8af848fe381d5f5423ca3f8c3e6be365e2333f5
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-08-30 07:35:44 +0300

    fix: tab navigation

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -17,36 +17,43 @@ const BurgerIngredients = (props) => {
     const sauces = ingredients.filter(item => item.type === 'sauce');
     const mains = ingredients.filter(item => item.type === 'main');
 
+    const onClickTabElement = (tab) => {
+        setCurrent(tab);
+        const element = document.getElementById(tab);
+        if (element) element.scrollIntoView({behavior: 'smooth'});
+      }
+    
+
     return (
         <section id="order-line" className={styles.section}>
             <h1 className="text text_type_main-medium pb-5 pt-10">Соберите бургер</h1>
             <div className={styles.tab}>
-                <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+                <Tab value="bun" active={current === 'bun'} onClick={onClickTabElement}>
                     Булки
                 </Tab>
-                <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+                <Tab value="sauce" active={current === 'sauce'} onClick={onClickTabElement}>
                     Соусы
                 </Tab>
-                <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+                <Tab value="main" active={current === 'main'} onClick={onClickTabElement}>
                     Начинки
                 </Tab>
             </div>
 
             <ul className={`${styles.ingredients} custom-scroll`}>
 
-                <h2 className="text text_type_main-medium pt-10 pb-6">Булки</h2>
+                <h2 id="bun" className="text text_type_main-medium pt-10 pb-6">Булки</h2>
                 <ul className={`${styles.list} pr-3`}>
                     {buns.map((item) => (
                         <BurgerCard card={item} name={item.name} price={item.price} image={item.image} key={item._id} />
                     ))}
                 </ul>
-                <h2 className="text text_type_main-medium pt-10 pb-6">Соусы</h2>
+                <h2 id="sauce" className="text text_type_main-medium pt-10 pb-6">Соусы</h2>
                 <ul className={`${styles.list} pr-3`}>
                     {sauces.map((item) => (
                         <BurgerCard card={item} name={item.name} price={item.price} image={item.image} key={item._id} />
                     ))}
                 </ul>
-                <h2 className="text text_type_main-medium pt-10 pb-6">Начинки</h2>
+                <h2 id="main" className="text text_type_main-medium pt-10 pb-6">Начинки</h2>
                 <ul className={`${styles.list} pr-3`}>
                     {mains.map((item) => (
                         <BurgerCard card={item} name={item.name} price={item.price} image={item.image} key={item._id} />
