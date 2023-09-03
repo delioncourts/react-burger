8a302ce3d88a20f4b3933ecc20f35f33e1b31d4c
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-03 14:10:20 +0300

    add: ingr detials, modal overlay fix

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -3,8 +3,36 @@ import styles from "./ingredient-details.module.css";
 
 const IngredientDetails = ({ ingredient }) => {
     return (
-        <>
-        </>
+        <div className={styles.container}>
+            <h2 className="text text_type_main-large">Детали ингредиента</h2>
+            <div className={styles.content}>
+                <img src={ingredient.image_large} alt={ingredient.name} />
+                <p className="text text_type_main-medium">{ingredient.name}</p>
+
+                <ul className={styles.list}>
+                    <li className={styles.itemslist}>
+                        <p className="text text_type_main-default">Калории,ккал</p>
+                        <p className="text text_type_digits-default pt-2">{ingredient.calories}</p>
+                    </li>
+
+                    <li className={styles.itemslist}>
+                        <p className="text text_type_main-default">Белки, г</p>
+                        <p className="text text_type_digits-default pt-2">{ingredient.proteins}</p>
+                    </li>
+
+                    <li className={styles.itemslist}>
+                        <p className="text text_type_main-default">Жиры, г</p>
+                        <p className="text text_type_digits-default pt-2">{ingredient.fat}</p>
+                    </li>
+
+                    <li className={styles.itemslist}>
+                        <p className="text text_type_main-default">Углеводы, г</p>
+                        <p className="text text_type_digits-default pt-2">{ingredient.carbohydrates}</p>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
     )
 }
 
