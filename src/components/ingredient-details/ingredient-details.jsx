cbed85194808c416ea3900d81be4073b6d78cae4
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-03 14:18:10 +0300

    fix: text color in ingr card

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -11,23 +11,23 @@ const IngredientDetails = ({ ingredient }) => {
 
                 <ul className={styles.list}>
                     <li className={styles.itemslist}>
-                        <p className="text text_type_main-default">Калории,ккал</p>
-                        <p className="text text_type_digits-default pt-2">{ingredient.calories}</p>
+                        <p className="text text_type_main-default text_color_inactive">Калории,ккал</p>
+                        <p className="text text_type_digits-default text_color_inactive pt-2">{ingredient.calories}</p>
                     </li>
 
                     <li className={styles.itemslist}>
-                        <p className="text text_type_main-default">Белки, г</p>
-                        <p className="text text_type_digits-default pt-2">{ingredient.proteins}</p>
+                        <p className="text text_type_main-default text_color_inactive">Белки, г</p>
+                        <p className="text text_type_digits-default text_color_inactive pt-2">{ingredient.proteins}</p>
                     </li>
 
                     <li className={styles.itemslist}>
-                        <p className="text text_type_main-default">Жиры, г</p>
-                        <p className="text text_type_digits-default pt-2">{ingredient.fat}</p>
+                        <p className="text text_type_main-default text_color_inactive">Жиры, г</p>
+                        <p className="text text_type_digits-default text_color_inactive pt-2">{ingredient.fat}</p>
                     </li>
 
                     <li className={styles.itemslist}>
-                        <p className="text text_type_main-default">Углеводы, г</p>
-                        <p className="text text_type_digits-default pt-2">{ingredient.carbohydrates}</p>
+                        <p className="text text_type_main-default text_color_inactive">Углеводы, г</p>
+                        <p className="text text_type_digits-default text_color_inactive pt-2">{ingredient.carbohydrates}</p>
                     </li>
                 </ul>
             </div>
