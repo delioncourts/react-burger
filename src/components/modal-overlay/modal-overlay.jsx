7838509497affe682f03556ac060f8799f585acc
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-03 14:34:46 +0300

    fix: close modal overlay

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import styles from "./modal-overlay.module.css";
 
-const ModalOverlay = () => {
+const ModalOverlay = ({children, onClose}) => {
     return (
-        <div className={styles.overlay}></div>
+        <div className={styles.overlay} onClick={onClose}>{children}</div>
     )
+}
+
+ModalOverlay.propTypes = {
+    onClose: PropTypes.func,
+    children: PropTypes.node,
 }
 
 export default ModalOverlay;
