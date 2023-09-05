9a02b70cfb5430f154dceab116e0cc9faf4e20db
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-05 10:34:32 +0300

    fix: styles order detail popup

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -4,10 +4,10 @@ import done from '../../images/done.svg';
 
 const OrderDetails = () => {
     return (
-        <div className={styles.popup}>
+        <div className={`${styles.popup} pt-30 pb-30`}>
             <h2 className={`${styles.glow} text text_type_digits-large`}>034536</h2>
             <p className="text text_type_main-medium pt-8 pb-15">идентификатор заказа</p>
-            <img src={done} alt='Ваш заказ начали готовить' />
+            <img className={styles.image} src={done} alt='Ваш заказ начали готовить' />
             <p className="text text_type_main-default pt-15">Ваш заказ начали готовить</p>
             <p className="text text_type_main-default text_color_inactive pt-2">Дождитесь готовности на орбитальной станции</p>
         </div>
