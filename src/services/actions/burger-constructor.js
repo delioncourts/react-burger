e0e3ec3675b72c0e28054174189f025338f7a31e
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-16 21:59:21 +0300

    add: actions burger constructor

---
+++ src/services/actions/burger-constructor.js
@@ -0,0 +1,6 @@
+//Получение списка ингредиентов для конструктора бургера
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_BUN_INGREDIENT = 'ADD_INGREDIENT';
+export const REMOVE_INGREDIENT = 'ADD_INGREDIENT';
+export const MOVE_INGREDIENT = 'ADD_INGREDIENT';
