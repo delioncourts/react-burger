3906f1cc5398966bf44be1c8fe454979b1c5b317
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-18 00:19:04 +0300

    fix: upd root reducer

--- src/services/reducers/rootreducer.js
+++ src/services/reducers/rootreducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { modalIngredientsReducer } from './modal';
 import { orderDetailsReducer } from './order-details';
 import { burgerIngredientsReducer } from './burger-ingredients';
+import { burgerConstructorReducer } from './burger-constructor';
 
 export const rootReducer = combineReducers({
   modal: modalIngredientsReducer,
   order: orderDetailsReducer,
   burgerIngredients: burgerIngredientsReducer,
+  burgerConstructor: burgerConstructorReducer,
 });
