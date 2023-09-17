6bc0d4df2d120a22ec46e8cbd07ead0f7f045d47
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-17 23:09:48 +0300

    fix: modal reducer and order details reducer

--- src/services/reducers/modal.js
+++ src/services/reducers/modal.js
@@ -10,29 +10,11 @@ export const modalIngredientsReducer = (state = initialState, action) => {
     case GET_VIEWED_INGREDIENT: {
       return { ...state, isOpen: true, currentIngredient: action.item };
     }
-    case REMOVE_VIEWED_INGREDIENTL: {
+    case REMOVE_VIEWED_INGREDIENT: {
       return { ...state, isOpen: false, currentIngredient: null };
     }
     default: {
       return state;
     }
-  }
-};
-export const ingredientDetailsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_INGREDIENTS_DETAILS: {
-      return {
-        ...state,
-        ingredientDetails: action.payload,
-      };
-    }
-    case DELETE_INGREDIENTS_DETAILS: {
-      return {
-        ...state,
-        ingredientDetails: null,
-      };
-    }
-    default:
-      return state;
   }
 };
--- src/services/reducers/order-details.js
+++ src/services/reducers/order-details.js
@@ -1,9 +1,48 @@
 //Получение и обновление номера заказа в модальном окне OrderDetails.
 
-import {SEND_ORDER_REQUEST, SEND_ORDER_ERROR, SEND_ORDER_SUCCESS, UPDATE_ORDER_NUMBER } from '../actions/order-details';
+import {
+  SEND_ORDER_REQUEST,
+  SEND_ORDER_ERROR,
+  SEND_ORDER_SUCCESS,
+  UPDATE_ORDER_NUMBER,
+} from '../actions/order-details';
 
 const initialState = {
-    orderNumber: 0,
-    orderRequest: false,
-    orderError: false,
+  orderNumber: 0,
+  orderRequest: false,
+  orderError: false,
+};
+
+export const orderDetailsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SEND_ORDER_REQUEST: {
+      return {
+        ...state,
+        orderRequest: true,
+        orderError: false,
+      };
+    }
+    case SEND_ORDER_SUCCESS: {
+      return {
+        ...state,
+        orderNumber: action.order.number,
+        orderRequest: false,
+      };
+    }
+    case SEND_ORDER_ERROR: {
+      return {
+        ...initialState,
+        orderError: true,
+      };
+    }
+    case UPDATE_ORDER_NUMBER: {
+      return {
+        ...state,
+        orderNumber: action.order.number,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 };
