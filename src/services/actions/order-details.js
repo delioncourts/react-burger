8288271a3b2672479a7a417e2c678e09af426b61
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-17 12:48:58 +0300

    add: order actions

--- src/services/actions/order-details.js
+++ src/services/actions/order-details.js
@@ -1,3 +1,35 @@
 //Получение и обновление номера заказа в модальном окне OrderDetails.
 
 import { createOrderRequest } from '../../utils/api';
+
+export const SEND_ORDER_REQUEST = "SEND_ORDER_REQUEST";
+export const SEND_ORDER_ERROR = "SEND_ORDER_ERROR";
+export const SEND_ORDER_SUCCESS = "SEND_ORDER_SUCCESS";
+
+export function sendOrder(arr) {
+    return function(dispatch) {
+        dispatch({
+            type: SEND_ORDER_REQUEST
+        });
+        createOrderRequest(arr)
+        .then(order => {
+            if(order.success) {
+                dispatch({
+                    type: SEND_ORDER_SUCCESS,
+                    orderNumber: order.order.number
+                })
+            }
+            else{
+                Promise.reject(`Произошла ошибка: ${order.status}`)
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({
+                type: SEND_ORDER_ERROR
+            })
+        });
+    }
+}
+
+
