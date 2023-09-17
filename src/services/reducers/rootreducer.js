ab14d822e562eb0777988190ac7293669df20894
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-17 23:10:41 +0300

    fix: rootreducer

--- src/services/reducers/rootreducer.js
+++ src/services/reducers/rootreducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { modalIngredientsReducer } from "./modal";
+import { orderDetailsReducer } from "./order-details";
 
 export const rootReducer = combineReducers({
 modal: modalIngredientsReducer,
+order: orderDetailsReducer,
 });
