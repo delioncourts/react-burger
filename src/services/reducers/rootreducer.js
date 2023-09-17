36169ff2e052f8d2d887f7333fee600c66c05376
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-09-17 23:20:02 +0300

    add: burger ingredients reducer, fix: root reducer

--- src/services/reducers/rootreducer.js
+++ src/services/reducers/rootreducer.js
@@ -1,8 +1,10 @@
-import { combineReducers } from "redux";
-import { modalIngredientsReducer } from "./modal";
-import { orderDetailsReducer } from "./order-details";
+import { combineReducers } from 'redux';
+import { modalIngredientsReducer } from './modal';
+import { orderDetailsReducer } from './order-details';
+import { burgerIngredientsReducer } from './burger-ingredients';
 
 export const rootReducer = combineReducers({
-modal: modalIngredientsReducer,
-order: orderDetailsReducer,
+  modal: modalIngredientsReducer,
+  order: orderDetailsReducer,
+  burgerIngredients: burgerIngredientsReducer,
 });
