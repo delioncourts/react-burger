1300daf26d63eca364efcc63735b734bb3c2dbe5
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-11-05 20:17:54 +0300

    feat: paths and functions in api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-import { getCookie } from "./cookie";
+import { getCookie } from './cookie';
 
 //объявляем базовый урл
 export const BASE_URL = 'https://norma.nomoreparties.space/api/';
@@ -41,4 +41,86 @@ export const createOrderRequest = (items) =>
     body: JSON.stringify({
       ingredients: items.map((item) => item._id),
     }),
+  });
+
+//регистрация
+export const register = (name, email, password) =>
+  request('auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify({ name, email, password }),
+  });
+
+//авторизация = login
+export const authorize = (email, password) =>
+  request('auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+//восстановление пароля по имейлу
+export const forgotPassword = (email) =>
+  request('password-reset', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify({ email }),
+  });
+
+//сбросить пароль
+export const resetPassword = (password, token) =>
+  request('password-reset/reset', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify({ password, token }),
+  });
+
+//выйти из профиля
+export const logout = (token) =>
+  request('auth/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+      Authorization: 'Bearer ' + getCookie('accessToken'),
+    },
+    body: JSON.stringify({ token }),
+  });
+
+//получить данные пользователя
+export const getUserInfo = (accessToken) =>
+  request('auth/user', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+      Authorization: accessToken,
+    },
+  });
+
+//обновить данные пользователя
+export const updateUserInfo = (accessToken, email, password, name) =>
+  request('auth/user', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+      Authorization: accessToken,
+    },
+    body: JSON.stringify({ email, password, name }),
+  });
+
+//обновить токен
+export const updateToken = (refreshToken) =>
+  request('auth/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify({ token: refreshToken }),
   });
