17577d14a1444a03eaf5dd5830a60e3aa6965c54
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-12-06 01:25:23 +0300

    refactor: order card and modal details

--- src/components/order-card/order-card.tsx
+++ src/components/order-card/order-card.tsx
@@ -1,11 +1,14 @@
 //карточки для ленты заказа 
-import { FormattedDate } from "@ya.praktikum/react-developer-burger-ui-components";
+
 import { useLocation } from "react-router";
 import { TOrderFeed } from "../../utils/types";
 import React from "react";
 import { FC } from "react";
 import { useSelector } from "../../index";
 import { AllIngredients } from "../../services/selectors";
+import styles from "./order-card.module.css";
+import { CurrencyIcon, FormattedDate } from "@ya.praktikum/react-developer-burger-ui-components";
+
 interface IOrderCard {
     order: TOrderFeed;
     status: boolean;
@@ -19,13 +22,29 @@ const OrderCard: FC<IOrderCard> = ({ order, status }) => {
 
     const ingredients = useSelector(AllIngredients);
 
-    const date = () => {
-        //return <FormattedDate date={new Date(el?.createdAt)} />
-    };
+    const totalPrice = 7;
+
+   /* function date() {
+        return <FormattedDate date={new Date(createdAt)} />
+    }*/
 
     return (
         <>
-            <p>la bla bla</p>b
+            <div className={`${styles.card} pt-6 pb-6`}>
+                <div className={`${styles.orderdate} pb-6`}>
+                    <p className="text text_type_main-small"></p>
+                    <p className="text text_type_main-default text_color_inactive"></p>
+                </div>
+
+                <p className="text text_type_main-medium pb-6"></p>
+
+                <div className={styles.ordercontent}>
+                    <div className={styles.price}>
+                        <p className="text text_type_digits-medium pr-1">{totalPrice || 0}</p>
+                        <CurrencyIcon type="primary" />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
