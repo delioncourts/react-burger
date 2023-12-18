bf4f52ff3ed872cf9652f636ce2d273ee088815f
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-12-18 12:17:48 +0300

    refactor

--- src/services/actions/order-details.ts
+++ src/services/actions/order-details.ts
@@ -52,20 +52,44 @@ export const sendOrder = (arr: TIngredientFull[]):AppThunk => (dispatch: AppDisp
 
 }
 
+export const GET_CURRENT_ORDER_REQUEST: 'GET_CURRENT_ORDER_REQUEST' = 'GET_CURRENT_ORDER_REQUEST';
+export const GET_CURRENT_ORDER_ERROR: 'GET_CURRENT__ORDER_ERROR' = 'GET_CURRENT__ORDER_ERROR';
+export const GET_CURRENT_ORDER_SUCCESS: 'GET_CURRENT_ORDER_SUCCESS' = 'GET_CURRENT_ORDER_SUCCESS';
+
+export interface IGetOrderCurrentRequestAction {
+  readonly type: typeof GET_CURRENT_ORDER_REQUEST;
+}
+
+export interface IGetOrderCurrentErrorAction {
+  readonly type: typeof GET_CURRENT_ORDER_ERROR;
+}
+
+export interface IGetOrderCurrentSuccessAction {
+  readonly type: typeof GET_CURRENT_ORDER_SUCCESS;
+  orders: TOrderFeed
+}
+
+
 export const getOrder = (number: string) => (dispatch: AppDispatch) => {
-  dispatch(GET_ORDER_REQUEST);
+  dispatch(GET_CURRENT_ORDER_REQUEST);
   getOrdersByNumber(number)
       .then((res) => {
         if (res.success) {
           dispatch({
-            type: GET_ORDER_SUCCESS,
+            type: GET_CURRENT_ORDER_SUCCESS,
             orders: res.orders[0],
           });
         } else {
           Promise.reject(`Произошла ошибка: ${res.status}`);
         }
       })
-      .catch((err) => dispatch(GET_ORDER_ERROR));
+      .catch((err) => dispatch(GET_CURRENT_ORDER_ERROR));
 }
 
-export type TOrderActions = IGetOrderRequestAction | IGetOrderErrorAction | IGetOrderSuccessAction | IUpdateOrderNumber;
+export type TOrderActions = IGetOrderRequestAction | 
+IGetOrderErrorAction | 
+IGetOrderSuccessAction | 
+IUpdateOrderNumber | 
+IGetOrderCurrentRequestAction |
+IGetOrderCurrentErrorAction |
+IGetOrderCurrentSuccessAction;
