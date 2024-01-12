10ea8e4403b15f8349cbcd3584ac34b228146771
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2024-01-12 20:05:02 +0300

    feat: web socket feed tests

--- src/services/reducers/jest-ws-feed.test.js
+++ src/services/reducers/jest-ws-feed.test.js
@@ -12,4 +12,48 @@ describe('check web socket feed reducer', () => {
   test('should return the initial (default) state', () => {
     expect(wsFeedReducer(undefined, {})).toEqual(defaultState);
   });
+
+  test('should start web socket connection', () => {
+    expect(wsFeedReducer(defaultState, { type: WS_FEED_CONNECTION_START })).toEqual({
+      ...defaultState,
+      wsConnecting: true,
+    });
+  });
+
+  test('should succesfully connect web socket', () => {
+    expect(wsFeedReducer(defaultState, { type: WS_FEED_CONNECTION_SUCCESS })).toEqual({
+      ...defaultState,
+      wsConnecting: false,
+      wsConnected: true,
+    });
+  });
+
+  test('should through error in web socket', () => {
+    expect(
+      wsFeedReducer(defaultState, {
+        type: WS_FEED_CONNECTION_ERROR,
+        payload: 'Connection error',
+      }),
+    ).toEqual({
+      ...defaultState,
+      error: 'Connection error',
+    });
+  });
+
+  test('should close web socket connection', () => {
+    expect(wsFeedReducer(defaultState, { type: WS_FEED_CONNECTION_CLOSED })).toEqual({
+      ...defaultState,
+      wsConnected: false,
+      wsConnecting: false,
+    });
+  });
+
+  test('should send web socket message', () => {
+    expect(
+      wsFeedReducer(defaultState, { type: WS_FEED_SEND_MESSAGE, payload: TEST_WS_ORDER }),
+    ).toEqual({
+      ...defaultState,
+      orders: TEST_WS_ORDER,
+    });
+  });
 });
